6ea2749dbc444f06df915db924f724fbb4d5c0ad
John Weru <john@Johns-MacBook-Pro.local>
2023-02-22 09:29:24 +0300

    intro to bootsrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
 
-import './App.css';
+import './index.css';
 import Employee from './components/Employee';
 import { useState } from 'react';
+import {v4  as uuidv4} from 'uuid';
 
 
 function App() {
   const [role, setRole] = useState('dev');
+  const [employees, setEmployee] = useState(
+    [
+      {
+      name:'caleb', 
+      role: 'developer', 
+      img: 'https://images.pexels.com/photos/5824213/pexels-photo-5824213.jpeg'
+    },
+    {
+      name:'caleb', 
+      role: 'developer', 
+      img: 'https://images.pexels.com/photos/5824213/pexels-photo-5824213.jpeg'
+    },
+    {
+      name:'caleb', 
+      role: 'developer', 
+      img: 'https://images.pexels.com/photos/5824213/pexels-photo-5824213.jpeg'
+    },
+    {
+      name:'caleb', 
+      role: 'developer', 
+      img: 'https://images.pexels.com/photos/5824213/pexels-photo-5824213.jpeg'
+    },
+    {
+      name:'caleb', 
+      role: 'developer', 
+      img: 'https://images.pexels.com/photos/5824213/pexels-photo-5824213.jpeg'
+    },
+    {
+      name:'caleb', 
+      role: 'developer', 
+      img: 'https://images.pexels.com/photos/5824213/pexels-photo-5824213.jpeg'
+    }
+
+    ]
+  )
   const showEmployee = true;
   return (
     <div className="App">
@@ -13,9 +49,18 @@ function App() {
         <>
         <input type = "text" onChange={(e) =>{console.log(e.target.value);
         setRole(e.target.value); }} />
-        <Employee name = 'John' role ='intern'/>
-        <Employee name = 'John' role ={role} />
-        <Employee name = 'John'/>
+              <div className='flex flex-wrap' justify-center >
+                 {employees.map( (employee )=> {
+                  console.log(uuidv4());
+                  return (<Employee
+                  key = {uuidv4()}
+                  name={employee.name}
+                  role={employee.role}
+                  img={employee.img}
+                  /> );
+                 })}
+
+              </div>
         </>) :
         <p>You dont have the rights</p>
      }
