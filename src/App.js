4713769372f9514ebd63cfdda45f22a337ef99ed
John Weru <john@Johns-MacBook-Pro.local>
2023-02-23 17:27:05 +0300

    end of modal

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './index.css';
 import Employee from './components/Employee';
 import { useState } from 'react';
 import {v4  as uuidv4} from 'uuid';
+import AddEmployee from './components/AddEmployee';
 
 
 function App() {
@@ -10,31 +11,36 @@ function App() {
   const [employees, setEmployee] = useState(
     [
       {
+      id: 1,
       name:'caleb', 
       role: 'developer', 
       img: 'https://images.pexels.com/photos/5824213/pexels-photo-5824213.jpeg'
     },
     {
+      id: 2,
       name:'caleb', 
       role: 'developer', 
       img: 'https://images.pexels.com/photos/5824213/pexels-photo-5824213.jpeg'
     },
     {
+      id: 3,
       name:'caleb', 
       role: 'developer', 
       img: 'https://images.pexels.com/photos/5824213/pexels-photo-5824213.jpeg'
     },
     {
+      id: 4,
       name:'caleb', 
       role: 'developer', 
       img: 'https://images.pexels.com/photos/5824213/pexels-photo-5824213.jpeg'
     },
     {
+      id: 5,
       name:'caleb', 
       role: 'developer', 
       img: 'https://images.pexels.com/photos/5824213/pexels-photo-5824213.jpeg'
     },
-    {
+    { id: 6,
       name:'caleb', 
       role: 'developer', 
       img: 'https://images.pexels.com/photos/5824213/pexels-photo-5824213.jpeg'
@@ -42,25 +48,48 @@ function App() {
 
     ]
   )
+  function updateEmployee(id, newName, newRole){
+   const updatedEmployees = employees.map((employee) => {
+    if (id == employee.id ) {
+      return {...employee, name : newName, role: newRole};
+    }
+    return employee;
+   });
+   setEmployee(updatedEmployees);
+  }
+
+  function newEmployee(name, role, img){
+   const newEmployee ={
+      id:uuidv4(),
+      name:name,
+      role:role,
+      img:img
+    }
+  setEmployee([...employees, newEmployee])}
   const showEmployee = true;
   return (
     <div className="App">
      {showEmployee ?(
         <>
-        <input type = "text" onChange={(e) =>{console.log(e.target.value);
+        <input type = "text" onChange={(e) =>{
         setRole(e.target.value); }} />
               <div className='flex flex-wrap' justify-center >
                  {employees.map( (employee )=> {
-                  console.log(uuidv4());
+                
                   return (<Employee
-                  key = {uuidv4()}
+
+                  key = {employee.id}
+                  id= {employee.id}
                   name={employee.name}
                   role={employee.role}
                   img={employee.img}
+                  updateEmployee = {updateEmployee}
+
                   /> );
                  })}
 
               </div>
+              <AddEmployee  newEmployee ={newEmployee}/>
         </>) :
         <p>You dont have the rights</p>
      }
